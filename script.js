315e8ae1d013e74b7a4ed44c10365d758718ef1a
JFNCGamer <78734362+JFNCGamer@users.noreply.github.com>
2021-03-07 09:40:42 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function TestFunction2() {
-    document.getElementById("test").innerHTML = "noob";
+    document.getElementById("test").innerHTML = "Teste";
   }
   function TestFunction() {
     document.getElementById("test").innerHTML = "";
