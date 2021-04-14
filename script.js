547df90c0266887413584a07d3f8a60958ba08f7
JFNCGamer <78734362+JFNCGamer@users.noreply.github.com>
2021-04-14 20:30:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function Function2() {
-    document.getElementById("conta").innerHTML = "Wrong!";
+    document.getElementById("conta").innerHTML = "Wrong";
   }
   function Function1() {
-    document.getElementById("conta").innerHTML = "Correct!";
+    document.getElementById("conta").innerHTML = "Correct";
   }
   function Function3() {
-    document.getElementById("conta2").innerHTML = "Correct!";
+    document.getElementById("conta2").innerHTML = "Correct";
   }
   function Function4() {
-    document.getElementById("conta2").innerHTML = "Wrong!";
+    document.getElementById("conta2").innerHTML = "Wrong";
   }
